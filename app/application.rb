ae477f12b3ab0fde601e40a105c2485ce8c251ab
github username <github email address>
2021-08-14 12:20:47 -0400

    removed commented code, ran seed file

--- app/application.rb
+++ app/application.rb
@@ -5,22 +5,21 @@ class Application
     req = Rack::Request.new(env)
 
     # CARD ROUTES 
+
     ## READ - Return all cards
     if req.path == '/cards' && req.get?
       cards = Card.all
       return [200, { 'Content-Type' => 'application/json' }, [ cards.to_json ]]
     end
+
     ## READ - Return a single card
     if req.path.match(/cards/) && req.get? 
-      # id = req.path.split('/')[2]
-      # card = Card.find_by(id: id)
       card = find_card(req.path)
       if card 
         return [200, { 'Content-Type' => 'application/json' }, [ card.to_json ]]
       else
         return [404, { 'Content-Type' => 'application/json' }, [ {message: "Card Not Found"}.to_json ]]
       end
-      
     end
 
     ## CREATE 
@@ -32,8 +31,6 @@ class Application
 
     ## UPDATE 
     if req.path.match(/cards/) && req.put?
-      # id = req.path.split('/')[2]
-      # card = Card.find_by(id: id)
       card = find_card(req.path)
       if card 
         body = JSON.parse(req.body.read)
@@ -42,18 +39,10 @@ class Application
       else
         return [404, { 'Content-Type' => 'application/json' }, [ {message: "Card Not Found"}.to_json ]]
       end
-      
-      # body = JSON.parse(req.body.read)
-      # card.update(body)
-      # return [200, { 'Content-Type' => 'application/json' }, [ card.to_json ]]
     end
 
     ## DELETE
     if req.path.match(/cards/) && req.delete?
-      # id = req.path.split('/')[2]
-      # card = Card.find_by(id: id)
-      # card.destroy
-      # return [200, { 'Content-Type' => 'application/json' }, [ {}.to_json ]]
       card = find_card(req.path)
       if card 
         card.destroy
@@ -77,18 +66,13 @@ class Application
     else
       res.write "Path Not Found"
     end
-
     res.finish
   end
 
   ## Helping Functions 
   def find_card(path)
     id = path.split('/')[2]
-    # card = Card.find_by(id: id)
     Card.find_by(id: id)
-    # card ? card : {message: "Card Not Found"}
   end
 
 end
-
-#Card.new(name: "Testing", design_img: 'Hi', build_cost: 1, establishment_id: 1, description: "Des")
